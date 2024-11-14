0ca4cb9e1aa1d42adfb106d95d043f905c0b83da
Pietro Nencini <nencinipietro2006@gmail.com>
2024-11-14 11:36:24 +0100

    completed

--- script.php
+++ script.php
@@ -1,6 +1,12 @@
 <?php 
     session_start();
-?>
+    if(!isset($_SESSION["maxPhr"]))
+        $_SESSION["maxPhr"] = [];
+    if(!isset($_SESSION["allChar"]))
+        $_SESSION["allChar"] = 0;
+    if(!isset($_SESSION["allWords"]))
+        $_SESSION["allWords"] = 0;
+?>  
 
 <!DOCTYPE html>
 <html lang="en">
@@ -17,10 +23,22 @@
     <h1 class="w-100 text-center bg-primary text-white"> DATI INSERITI </h1>
 
     <?php 
-        // Add PHP code here...
+        $inserted_phrase = $_POST["phrase"];
+        $priority = $_POST["priority"];
 
+        if($priority == 4)
+            array_push($_SESSION["maxPhr"], $inserted_phrase);
+
+        $_SESSION["allChar"] += strlen($inserted_phrase);
+        $_SESSION["allWords"] += str_word_count($inserted_phrase);
     ?>
 
+    <div class="w-50 mx-auto text-center">
+        <p class="fs-5"> Frase inserita: <br> <span style="color: green;"> <?php echo $inserted_phrase ?> </span> </p>
+        <p> Importanza della frase: <?php echo $priority ?>/5  </p>
+    </div>
+
+    <a href="index.html" target="_blank" class="link fs-4 d-block w-50 mx-auto"> Torna alla Home Page </a>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 </body>
--- summary.php
+++ summary.php
@@ -14,13 +14,41 @@
 </head>
 <body>
     
-    <h1 class="w-100 text-center bg-primary text-white"> DATI INSERITI </h1>
+    <h1 class="w-100 text-center bg-primary text-white"> RIEPILOGO </h1>
 
     <?php 
-        // Add PHP code here...
-
+        if(!isset($_SESSION["maxPhr"]) || !isset($_SESSION["allChar"]) || !isset($_SESSION["allWords"])) {
+            echo "Nessuna frase ancora inserita";
+            exit();
+        }
     ?>
 
+    <div class="w-75 mx-auto bg-body-secondary shadow-lg my-3 p-3">
+        <p>  </p>
+        <ul style="list-style-type: none; color: green">
+            <?php
+                if(count($_SESSION["maxPhr"]) > 0) {
+                    foreach($_SESSION["maxPhr"] as $phrase) {
+                        echo "<li> $phrase </li> ";
+                    }
+                } else  
+                    echo "<li style='color: black;'> Nessuna frase con importanza massima inserita </li>";
+            ?>
+        </ul>
+    </div>
+    <div class="w-75 mx-auto bg-body-secondary shadow-lg my-3 p-3">
+        <p> Dati su tutte frasi inserite finora </p>
+        <table class="table rounded-3">
+            <tr>
+                <th>Numero di parole</th>
+                <th>Numero di caratteri totali</th>
+            </tr>
+            <tr>
+                <td><?php echo $_SESSION["allWords"] ?></td>
+                <td><?php echo $_SESSION["allChar"] ?></td>
+            </tr>
+        </table>
+    </div>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 </body>
